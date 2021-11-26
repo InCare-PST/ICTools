9146008b7ce300b8d99c4cd831c11720054f4769
Drethos <47424284+Drethos@users.noreply.github.com>
2021-11-26 13:26:37 -0600

    Moving to invoke-command

--- PS Scripts/Get-ServiceAccounts.ps1
+++ PS Scripts/Get-ServiceAccounts.ps1
@@ -1,13 +1,20 @@
 ﻿Function Get-ServiceAccounts{
     [cmdletbinding()]
         param(
+
+            [switch]$export,
+
+            [string]$path = "C:\temp",
+
+            [string]$username = "Administrator"
             
         )
     Begin{
         Write-Verbose "Getting list of online servers"
+        $date = (get-date).AddDays(-60)
         $onlineservers = @()
         $offlineservers = @()
-        $allservers = Get-ADComputer -filter * -Properties operatingsystem | where {$_.operatingsystem -match "server" -and $_.enabled -eq $true}
+        $allservers = Get-ADComputer -filter * -Properties operatingsystem,lastlogondate | Where-Object {$_.operatingsystem -match "server" -and $_.enabled -eq $true -and $_.lastlogondate -ge $date}
         foreach ($server in $allservers){
             if (Test-Connection $server.name -Count 1 -Quiet){
                 $onlineservers += $server
@@ -18,6 +25,19 @@
         }
     }
     Process{
+        $serverNameList = $onlineservers.Name
+        $serviceList = Invoke-Command -ComputerName $serverNameList {
+            Get-WmiObject win32_service | Select-Object systemName,displayname,startname
+        }
+        $finallist = $serviceList | Where-Object {$_.startname -match $username} | Sort-Object -Property systemName
+        if ($export){
+             $finallist | Export-Csv -Path $path -NoTypeInformation 
+        }
+        else {
+            $finallist
+        }
+
+        <# Commenting out, going with Invoke-Command
         $dcomopt = New-CimSessionOption -Protocol Dcom
         $wsmanopt = New-CimSessionOption -Protocol Wsman
         $wsmanerrors = @()
@@ -45,7 +65,7 @@
             }
         }
         $services = Get-CimInstance -CimSession (Get-CimSession) -ClassName win32_service | where {$_.startname -notmatch "local|Network\sService|NetworkService" -and $_.StartName -ne $null}
-    
+    #>
     }
     End{
     
