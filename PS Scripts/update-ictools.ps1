5e764c71aa7097db428e93ad21129ffc0fcbea22
Drethos <47424284+Drethos@users.noreply.github.com>
2022-05-12 10:22:31 -0500

    updating update-ictools script

--- PS Scripts/update-ictools.ps1
+++ PS Scripts/update-ictools.ps1
@@ -1,6 +1,8 @@
-$url = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools.psm1"
+$psmurl = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools.psm1"
+$psdurl = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools.psd1"
 $wc = New-Object System.Net.WebClient
-Get-FileHash -InputStream ($wc.openread($url)) -Algorithm MD5
+Get-FileHash -InputStream ($wc.openread($psmurl)) -Algorithm MD5
+Get-FileHash -InputStream ($wc.openread($psdurl)) -Algorithm MD5
 
 $modulepaths = $env:PSModulePath.Split(";")
 $instance = 0
@@ -17,6 +19,10 @@ foreach($mpath in $modulepaths){
         }
     }
 }
+if(![bool]$installpath){
+    $installpath = "$Home\Documents\WindowsPowerShell\Modules\"
+}
+
 
 
 
@@ -30,40 +36,78 @@ Function Update-ICTools {
 
 
 Begin{
-
     if($Beta){
     #Beta Variables
-        $url = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools-Beta.psm1"
+        $psmurl = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools-Beta.psm1"
+        $psdurl = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools-Beta.psd1"
             }else{
     #Production Variables
-        $url = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools.psm1"
-
+        $psmurl = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools.psm1"
+        $psdurl = "https://raw.githubusercontent.com/InCare-PST/ICTools/master/Modules/ICTools/ICTools.psd1"
             }
-
-    #Constant Variables
-        if (Test-Path $Home\Documents\WindowsPowerShell\Modules\ICtools) {
-            <# Action to perform if the condition is true #>
+    #Determine current ICTools path
+    $modulepaths = $env:PSModulePath.Split(";")
+    $instance = 0
+    foreach($mpath in $modulepaths){
+        if(test-path $mpath\ICTools\ictools.psm1){
+            $instance = $instance + 1
+            if($instance -le 1){
+                $installpath = $mpath
+            }
+            elseif ($instance -gt 1) {
+                write-host "ICTools Module found in multiple locations" -ForegroundColor Red
+                Write-Host "$installpath and in $mpath" -ForegroundColor Yellow
+                Exit
+            }
         }
+    }
+    if(![bool]$installpath){
+        $installpath = "$Home\Documents\WindowsPowerShell\Modules\"
+    }
+    #Get MD5 hash of online files
+    $wc = New-Object System.Net.WebClient
+    $psmhash = Get-FileHash -InputStream ($wc.openread($psmurl)) -Algorithm MD5 -ErrorAction Stop
+    $psdhash = Get-FileHash -InputStream ($wc.openread($psdurl)) -Algorithm MD5 -ErrorAction Stop
 
-        $ictpath = "$Home\Documents\WindowsPowerShell\Modules\ICTools"
-        $file = "$ictpath\ICTools.psm1"
-        $bakfile = "$ictpath\ICtools.bak"
-        $temp = "$ictpath\ICTools.temp.psm1"
-        $webclient = New-Object System.Net.WebClient
-        $psptest = Test-Path $Profile
-        $psp = New-Item –Path $Profile –Type File –Force
+    $ictpath = "$installpath\ICTools"
+    $psmfile = "$ictpath\ICTools.psm1"
+    $psdfile = "$ictpath\ICTools.psd1"
+    $bakfile = "$ictpath\ICtools.bak"
+    #$temp = "$ictpath\ICTools.temp.psm1"
+    #$webclient = New-Object System.Net.WebClient
+    $psptest = Test-Path $Profile
+    $psp = New-Item -Path $Profile -Type File -Force
+
+    #get the file hash for existing files
+    $cpsmhash = Get-FileHash -Path $psmfile -Algorithm MD5
+    $cpsdhash = Get-FileHash -Path $psdfile -Algorithm MD5
 }
 Process{
-#Make Directories
+    #compare files and replace if necessary 
+    if(!($psmhash.hash -eq $cpsmhash.hash)){
+        remove-item $psmfile -Force
+        $wc.DownloadFile($psmurl,$psmfile)
+    }
+    if(!($psdhash.hash -eq $cpsdhash.hash)){
+        remove-item $psdfile -Force
+        $wc.DownloadFile($psdurl,$psdfile)
+    }
 
-if(!(Test-Path -Path $ictpath)){New-Item -Path $ictpath -Type Directory -Force}
-if(!$psptest){$psp}
-#if(!(Test-Path -Path $archive)){New-Item -Path $archive}
 
-if(Test-Path -Path $bakfile){Remove-Item -Path $bakfile -Force}
-if(Test-Path -Path $file){Rename-Item -Path $file -NewName $bakfile -Force}
 
-$webclient.downloadfile($url, $file)
+
+
+
+    #Make Directories
+
+    if(!(Test-Path -Path $ictpath)){New-Item -Path $ictpath -Type Directory -Force}
+    if(!$psptest){$psp}
+    #if(!(Test-Path -Path $archive)){New-Item -Path $archive}
+
+    if(Test-Path -Path $bakfile){Remove-Item -Path $bakfile -Force}
+    if(Test-Path -Path $file){Rename-Item -Path $file -NewName $bakfile -Force}
+
+    $webclient.downloadfile($url, $file)
 }
 End{
 #Planned for Version number check to temp and only update if not latest version
