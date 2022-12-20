ad24c63213f19afc5930a555fa1a193c9f1a8331
Drethos <47424284+Drethos@users.noreply.github.com>
2022-12-20 07:56:53 -0600

    updated script

--- PS Scripts/update-clientinfo.ps1
+++ PS Scripts/update-clientinfo.ps1
@@ -12,26 +12,32 @@ function update-client {
     Process{
         foreach($user in $userinfo){
             $adaccount = Get-ADUser -Filter *  | Where-Object {$_.UserPrincipalName -match $user.userid}
-            if($user.enabled -eq "N"){
-                if($adaccount.enabled -eq $true){
-                   #$adaccount | Set-ADUser -Enabled $false
+            if([bool]$adaccount){
+                if($user.enabled -eq "N"){
+                    if($adaccount.enabled -eq $true){
+                    #$adaccount | Set-ADUser -Enabled $false
+                    }
+                    else{}
+                }
+                else{
+                    #if(([bool]$user.Mobilephone) -and ($user.Mobilephone -notmatch $adaccount.MobilePhone)){
+                        if([bool]$user.Mobilephone){
+                            #$adaccount | Set-ADUser -MobilePhone $user.Mobilephone
+                            $adaccount | Select-Object name
+                            $props = @{
+                                username = $adaccount.name
+                                snowname = $user.name
+                                enabled = $adaccount.enabled
+                                currentmobile = $adaccount.mobilephone
+                                newmobile = $user.Mobilephone
+                            }
+                            $tempobject = New-Object psobject -Property $props
+                            $tempobject | Export-Csv -Path c:\temp\mobileupdate.csv -NoTypeInformation -Append
+                        }
                 }
             }
             else{
-                #if(([bool]$user.Mobilephone) -and ($user.Mobilephone -notmatch $adaccount.MobilePhone)){
-                    if([bool]$user.Mobilephone){
-                        #$adaccount | Set-ADUser -MobilePhone $user.Mobilephone
-                        $adaccount | Select-Object name
-                        $props = @{
-                            username = $adaccount.name
-                            snowname = $user.name
-                            enabled = $adaccount.enabled
-                            currentmobile = $adaccount.mobilephone
-                            newmobile = $user.Mobilephone
-                        }
-                        $tempobject = New-Object psobject -Property $props
-                        $tempobject | Export-Csv -Path c:\temp\mobileupdate.csv -NoTypeInformation -Append
-                    }
+                Write-Host "Cannot find AD account for $($user.name)" -ForegroundColor Red
             }
         }
     }
