8cae1307b68bee5103ff7dc8023243342d78fdd2
Drethos <47424284+Drethos@users.noreply.github.com>
2022-12-20 10:27:45 -0600

    update script

--- PS Scripts/update-clientinfo.ps1
+++ PS Scripts/update-clientinfo.ps1
@@ -2,42 +2,55 @@ function update-client {
     [CmdletBinding()]
         Param (
 
-            [string]$path = "c:\temp\clientupdate.csv"
+            [string]$path = "c:\temp\",
+
+            [string]$filename = "clientupdate.csv",
+
+            [switch]$disable = $false
             
         )
     Begin{
-        $userinfo = Import-Csv -Path $path
+        $userinfo = Import-Csv -Path $path\$filename
+        $adusers = Get-ADUser -Filter *
         #$olduserinfo = Get-ADUser -Properties * -Filter *
     }
     Process{
         foreach($user in $userinfo){
-            $adaccount = Get-ADUser -Filter *  | Where-Object {$_.UserPrincipalName -match $user.userid}
+            $adaccount = $adusers | Where-Object {$_.UserPrincipalName -match $user.userid}
             if([bool]$adaccount){
                 if($user.enabled -eq "N"){
-                    if($adaccount.enabled -eq $true){
-                    #$adaccount | Set-ADUser -Enabled $false
+                    if(($adaccount.enabled -eq $true) -and ($disable -eq $false)){
+                        $adaccount | Select-Object name,userid,enabled | Export-Csv -Path $path\accountstodisable.csv -NoTypeInformation -Append
                     }
-                    else{}
+                    elseif(($adaccount.enabled -eq $true) -and ($disable -eq $true)){
+                        $adaccount | Set-ADUser -Enabled $false
+                    }
                 }
                 else{
                     #if(([bool]$user.Mobilephone) -and ($user.Mobilephone -notmatch $adaccount.MobilePhone)){
-                        if([bool]$user.Mobilephone){
+                        if(([bool]$user.Mobilephone) -and !($user.Mobilephone -notmatch $adaccount.MobilePhone)){
                             #$adaccount | Set-ADUser -MobilePhone $user.Mobilephone
+                            $tempmobile = $user.mobilephone -replace "\D+"
+                            $newmobile = "{0:(###) ###-####}" -f [int]$tempmobile
                             $adaccount | Select-Object name
                             $props = @{
                                 username = $adaccount.name
                                 snowname = $user.name
                                 enabled = $adaccount.enabled
                                 currentmobile = $adaccount.mobilephone
-                                newmobile = $user.Mobilephone
+                                newmobile = $newmobile
                             }
                             $tempobject = New-Object psobject -Property $props
-                            $tempobject | Export-Csv -Path c:\temp\mobileupdate.csv -NoTypeInformation -Append
+                            $tempobject | Export-Csv -Path $path\mobileupdate.csv -NoTypeInformation -Append
                         }
                 }
             }
             else{
-                Write-Host "Cannot find AD account for $($user.name)" -ForegroundColor Red
+
+                Write-Host "Cannot find AD account for $($user.name) with user id $($user.userid)" -ForegroundColor Red
+
+                $user | Export-Csv -Path $path\noaccount.csv -NoTypeInformation -Append
+
             }
         }
     }
