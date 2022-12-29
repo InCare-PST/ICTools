0903cd824231a7f6c9da7cbfc9ba654202991259
Drethos <47424284+Drethos@users.noreply.github.com>
2022-12-29 14:08:18 -0600

    moving from script to function

--- PS Scripts/SNOW Contacts/compare-contacts.ps1
+++ PS Scripts/SNOW Contacts/compare-contacts.ps1
@@ -1,14 +1,40 @@
-$clientsnow = Import-Csv -Path c:\temp\clientsnow.csv
-$adusers = Get-ADUser -Filter * -Properties enabled,mail
-$approved = $clientsnow | Where-Object {$_.u_approval_status -eq "Approved"}
-foreach($user in $approved){
-    $useraccount = $adusers | Where-Object {$_.mail -match $user.email}
-    $props = @{
-        Name = $useraccount.Name
-        SNOWName = $user.name
-        Enabled = $useraccount.enabled
-        Approved = $user.u_approval_status
+function Compare-SnowContacts {
+    [CmdletBinding()]
+    param (
+        [string]$path = "c:\temp",
+
+        [string]$file = "SnowExport.csv",
+
+        [string]$clientname
+
+    )
+    begin {
+        if (!(Test-Path $path\$file)) {
+            Write-Error "$($path)\$($file) does not exist. Script cannot proceed without source information."
+            Exit
+        }
+        else {
+            $SnowInfo = Import-Csv -Path $path\$file
+        }
+        if (![bool]$clientname) {
+            $clientname = Read-Host "Please enter client name."
+        }
+        $adusers = Get-ADUser -Filter * -Properties enabled, mail
+        $approved = $SnowInfo | Where-Object { $_.u_approval_status -eq "Approved" }
     }
-    $tempobject = New-Object -TypeName psobject -Property $props
-    $tempobject | Where-Object {$_.enabled -eq $false -and $_.Approved -eq "Approved"} |Select-Object Name,SNOWName,Enabled,Approved | Export-Csv -Path C:\Temp\SnowCompare02.csv -NoTypeInformation -Append
+    process {
+        foreach ($user in $approved) {
+            $useraccount = $adusers | Where-Object { $_.mail -match $user.email }
+            $props = @{
+                Name     = $useraccount.Name
+                SNOWName = $user.name
+                Enabled  = $useraccount.enabled
+                Approved = $user.u_approval_status
+            }
+            $tempobject = New-Object -TypeName psobject -Property $props
+            $tempobject | Where-Object { $_.enabled -eq $false -and $_.Approved -eq "Approved" } | Select-Object Name, SNOWName, Enabled, Approved | Export-Csv -Path "$($path)\$($clientname)"+"-SnowCompare.csv" -NoTypeInformation -Append
+        }
+        end {
+        }
+    }
 }
