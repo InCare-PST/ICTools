552b0ed1cf5f034dc81a72f0fcbd127687faf925
Michael Watts <mwatts@thrivenetworks.com>
2023-01-27 07:58:16 -0600

    Signed-off-by: Michael Watts <mwatts@thrivenetworks.com>

--- PS Scripts/Push Windows Updates.ps1
+++ PS Scripts/Push Windows Updates.ps1
@@ -1,10 +1,12 @@
 $computerlist = Get-Content -Path C:\scripts\servers.txt
 $i = 1200
 
+#gets server list, previous boot time
 Get-CimInstance -ClassName win32_operatingsystem -ComputerName $computerlist | select csname, buildnumber, lastbootuptime | Sort-Object -Property lastbootuptime
 Invoke-WUInstall -ComputerName $computerlist -Script {ipmo PSWindowsUpdate; Get-WUInstall –MicrosoftUpdate -AcceptAll -autoreboot | out-file c:\PSWindowsUpdate.log } -Confirm:$False -Verbose
 For ($i -gt 1; $i–-) {  
     Write-Progress -Activity "Servers Updating" -SecondsRemaining $i
     Start-Sleep 1
 }
+#gets server list new boot time after updates installed
 Get-CimInstance -ClassName win32_operatingsystem -ComputerName $computerlist | select csname, buildnumber, lastbootuptime | Sort-Object -Property lastbootuptime
