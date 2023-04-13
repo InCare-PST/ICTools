af85a576bc1612342ac2fa136b8158a001113b5a
BigDogJT <justin@gallups.net>
2023-04-13 11:35:02 -0500

    commit

--- PS Scripts/fix-keepervault.ps1
+++ PS Scripts/fix-keepervault.ps1
@@ -1,13 +1,29 @@
 param (
-  [switch]$backup
+  [Parameter(Mandatory=$true,
+  ValueFromPipeline=$true)]
+  [switch]$backup,
+  [switch]$remove,
+  [switch]$repair
 )
 
 $ext = "$env:userprofile\AppData\Local\Microsoft\Edge\User Data\Default\Extensions\lfochlioelphaglamdcakfjemolpichk"
 $bak = "C:\backups\keepervault"
 
 if([bool]$backup){robocopy $ext $bak /MIR /E}
-else{
-    Remove-Item $ext -Recurse -Force -Verbose
-    invoke-command -ScriptBlock {robocopy $bak $ext /MIR /E}
+
+
+if([bool]$remove){
+  Remove-Item $ext -Force -Recurse
+  taskkill /f /im:*edge*
 }
+
+if([bool]$repair){
+  if(Test-Path $bak){ 
+  Remove-Item $ext -Recurse -Force -Verbose
+  taskkill /f /im:*edge*
+  invoke-command -ScriptBlock {robocopy $bak $ext /MIR /E}}
+  else{
+    write-host "The is no backup to restore" -ForegroundColor Red
+    Start-Sleep 5
+  }}
 
