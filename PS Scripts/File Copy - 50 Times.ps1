60b5bf6ce05091eb7074442289a143edf0ac32c5
BigDogJT <justin@gallups.net>
2023-08-04 10:53:25 -0500

    Copy File

---
+++ PS Scripts/File Copy - 50 Times.ps1
@@ -0,0 +1,4 @@
+﻿for($num = 1 ; $num -le 50 ; $num++){
+    Start-process -FilePath C:\Windows\system32\Robocopy.exe -ArgumentList  "\\10.255.255.11\SHARED D:\SHARED\ /E /B /COPYALL /R:6 /W:5 /MT:64 /XD DfsrPrivate /TEE /XF *.pst ~$* ~*.tmp /XD LASERFICHE" -wait -WindowStyle Minimized
+    write-host -ForegroundColor Green "Finished $num loops"
+    }
