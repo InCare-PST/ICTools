fbb5ff861b17c9123b6beef83896fce072e36e03
Drethos <47424284+Drethos@users.noreply.github.com>
2023-12-11 08:26:54 -0600

    Modified Scripts

--- PS Scripts/get-MailboxData.ps1
+++ PS Scripts/get-MailboxData.ps1
@@ -1,85 +1,89 @@
-# Install the required modules
-Install-Module -Name Microsoft.Graph.Authentication -Force -AllowClobber
+function Get-TenantInfo{
+    param(
 
-# Import the required modules
-Import-Module Microsoft.Graph.Authentication
+        [string]$path = "C:\temp",
 
-# Your app registration details
-$clientID = "your-client-id"
-$tenantID = "your-tenant-id"
-$scope = "User.Read.All,Organization.Read.All"  # Specify the "User.Read" scope for basic user profile read access
+        [string]$clientname = "",
 
-# Connect to Microsoft Graph using Connect-MgGraph with specified scope
-Connect-MgGraph -ClientId $clientID -TenantId $tenantID -Scopes $scope
+        [string]$scope = "User.Read.All,Organization.Read.All"
+    )
 
-# Define the URL of the mapping file hosted online
-$mappingFileUrl = "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv"
+    # Your app registration details
+    $clientID = "your-client-id"
+    $tenantID = "your-tenant-id"
+    $scope = "User.Read.All,Organization.Read.All"  # Specify the "User.Read" scope for basic user profile read access
 
-# Download the mapping file
-Invoke-RestMethod -Uri $mappingFileUrl -OutFile "C:\temp\MappingFile.csv"
+    # Connect to Microsoft Graph using Connect-MgGraph with specified scope
+    Connect-MgGraph -ClientId $clientID -TenantId $tenantID -Scopes $scope
 
-# Load the mapping from the downloaded CSV file
-$planMapping = Import-Csv "C:\temp\MappingFile.csv"
+    # Define the URL of the mapping file hosted online
+    $mappingFileUrl = "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv"
 
-# Step 2: Retrieve information for each mailbox
-$mailboxes = Get-MgUser -All
+    # Download the mapping file
+    Invoke-RestMethod -Uri $mappingFileUrl -OutFile "C:\temp\MappingFile.csv"
 
-# Step 3: Format and export the information
-$exportedData = foreach ($mailbox in $mailboxes) {
-    $licenses = $mailbox.assignedlicenses.skuid
-    $assignedlicenses = @()
-    foreach($license in $licenses){
-        $friendlyname = $planmapping | Where-Object {$_.GUID -eq $license} | Select-Object -First 1 | Select-Object Product_Display_Name
-        $assignedlicenses += $friendlyname
+    # Load the mapping from the downloaded CSV file
+    $planMapping = Import-Csv "C:\temp\MappingFile.csv"
+
+    # Step 2: Retrieve information for each mailbox
+    $mailboxes = Get-MgUser -All
+
+    # Step 3: Format and export the information
+    $exportedData = foreach ($mailbox in $mailboxes) {
+        $licenses = $mailbox.assignedlicenses.skuid
+        $assignedlicenses = @()
+        foreach($license in $licenses){
+            $friendlyname = $planmapping | Where-Object {$_.GUID -eq $license} | Select-Object -First 1 | Select-Object Product_Display_Name
+            $assignedlicenses += $friendlyname
+        }
+        $liclist = $assignedlicenses.Product_Display_Name -join "+"
+        $officephone = $mailbox.BusinessPhones -join ","
+        
+        [PSCustomObject]@{
+            DisplayName = $mailbox.displayName
+            FirstName = $mailbox.givenName
+            LastName = $mailbox.surname
+            Enabled = $mailbox.AccountEnabled
+            "Sync Enabled" = $mailbox.OnPremisesSyncEnabled
+            UserType = $mailbox.userType
+            Licenses = $liclist  # Use the translated friendly names
+            "User Principal Name" = $mailbox.UserPrincipalName
+            "Street Address" = $mailbox.StreetAddress
+            City = $mailbox.City
+            State = $mailbox.State
+            "Postal Code" = $mailbox.PostalCode
+            "Country/Region" = $mailbox.Country
+            Department = $mailbox.Department
+            "Office Name" = $mailbox.OfficeLocation
+            "Office Phone" = $officephone
+            "Mobile Phone" = $mailbox.MobilePhone
+            "When Created" = $mailbox.CreatedDateTime
+        }
     }
-    $liclist = $assignedlicenses.Product_Display_Name -join "+"
-    $officephone = $mailbox.BusinessPhones -join ","
-    
-    [PSCustomObject]@{
-        DisplayName = $mailbox.displayName
-        FirstName = $mailbox.givenName
-        LastName = $mailbox.surname
-        Enabled = $mailbox.AccountEnabled
-        "Sync Enabled" = $mailbox.OnPremisesSyncEnabled
-        UserType = $mailbox.userType
-        Licenses = $liclist  # Use the translated friendly names
-        "User Principal Name" = $mailbox.UserPrincipalName
-        "Street Address" = $mailbox.StreetAddress
-        City = $mailbox.City
-        State = $mailbox.State
-        "Postal Code" = $mailbox.PostalCode
-        "Country/Region" = $mailbox.Country
-        Department = $mailbox.Department
-        "Office Name" = $mailbox.OfficeLocation
-        "Office Phone" = $officephone
-        "Mobile Phone" = $mailbox.MobilePhone
-        "When Created" = $mailbox.CreatedDateTime
+
+    # Export the data to a CSV file
+    $exportedData | Export-Csv -Path "C:\Path\To\Exported\MailboxInfo.csv" -NoTypeInformation
+
+    Import-Module Microsoft.Graph.beta.Identity.DirectoryManagement
+
+    $subskus = Get-MgBetaSubscribedSku -All
+
+    $subscribtionexport = foreach($subsku in $subskus){
+        $basesku = $subsku.skuid
+        $friendlyname = $planMapping | Where-Object {$_.GUID -eq $basesku} | Select-Object -First 1 | Select-Object -ExpandProperty Product_Display_Name
+
+        #$friendlyname = $friendlyname.Product_Display_Name
+
+        [PSCustomObject]@{
+            License = $friendlyname
+            Enabled = $subsku.PrepaidUnits.Enabled
+            Assigned = $subsku.ConsumedUnits
+            Expired = $subsku.PrepaidUnits.Suspended
+            Available = $subsku.prepaidunits.enabled - $subsku.consumedUnits
+        }
     }
+    $subscribtionexport | Where-Object {$_.License -ne $null} |  export-csv -Path C:\temp\Stansell_Licenses.csv -NoTypeInformation -Force
 }
-
-# Export the data to a CSV file
-$exportedData | Export-Csv -Path "C:\Path\To\Exported\MailboxInfo.csv" -NoTypeInformation
-
-Import-Module Microsoft.Graph.beta.Identity.DirectoryManagement
-
-$subskus = Get-MgBetaSubscribedSku -All
-
-$subscribtionexport = foreach($subsku in $subskus){
-    $basesku = $subsku.skuid
-    $friendlyname = $planMapping | Where-Object {$_.GUID -eq $basesku} | Select-Object -First 1 | Select-Object -ExpandProperty Product_Display_Name
-
-    #$friendlyname = $friendlyname.Product_Display_Name
-
-    [PSCustomObject]@{
-        License = $friendlyname
-        Enabled = $subsku.PrepaidUnits.Enabled
-        Assigned = $subsku.ConsumedUnits
-        Expired = $subsku.PrepaidUnits.Suspended
-        Available = $subsku.prepaidunits.enabled - $subsku.consumedUnits
-    }
-}
-
-$subscribtionexport | Where-Object {$_.License -ne $null} |  export-csv -Path C:\temp\Stansell_Licenses.csv -NoTypeInformation -Force
 
 function Export-TenantUsers {
     param(
