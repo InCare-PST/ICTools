e96e722eb09c16259926db93ef32c98d4da14b8e
BigDogJT <justin@gallups.net>
2024-06-05 10:25:52 -0500

    oops, forgot to add the auth method to the box

--- PS Scripts/Graph-MFAStatus.ps1
+++ PS Scripts/Graph-MFAStatus.ps1
@@ -1,7 +1,7 @@
 
 # Connect to Microsoft Graph
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
-$scope = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Scope Requested", "Scope", "User.Read.All,Organization.Read.All,AuditLog.Read.All,Directory.Read.All")
+$scope = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Scope Requested", "Scope", "User.Read.All,Organization.Read.All,AuditLog.Read.All,Directory.Read.All,UserAuthenticationMethod.Read.All")
 #$scope = "User.Read.All,Organization.Read.All,AuditLog.Read.All,Directory.Read.All"
 Connect-MgGraph -Scopes $scope
 
