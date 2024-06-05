432432ddd696f1a5b2280f227a920393b5838b25
BigDogJT <justin@gallups.net>
2024-06-05 09:13:19 -0500

    optimize script

--- PS Scripts/Graph-MFAStatus.ps1
+++ PS Scripts/Graph-MFAStatus.ps1
@@ -4,34 +4,30 @@ Write-Host "Finding Azure Active Directory Accounts..."
 Connect-MgGraph -Scopes "User.Read.All"
 
 # Get all users excluding guest users
-$Users = Get-MgUser -Filter "userType ne 'Guest' and accountEnabled eq true" -Property "id, displayName, userPrincipalName, strongAuthenticationMethods, proxyAddresses, strongAuthenticationUserDetails"
+$Users = Get-MgUser -Filter "userType ne 'Guest' and accountEnabled eq true" -Property "id, displayName, userPrincipalName, strongAuthenticationMethods, proxyAddresses, strongAuthenticationUserDetails, strongAuthenticationRequirements" -All
 
 $Report = [System.Collections.Generic.List[Object]]::new() # Create output file
-Write-Host "Processing" $Users.Count "accounts..." 
+Write-Host "Processing" $Users.Count "accounts..."
+
 ForEach ($User in $Users) {
     $MFADefaultMethod = ($User.StrongAuthenticationMethods | Where-Object { $_.IsDefault -eq "True" }).MethodType
     $MFAPhoneNumber = $User.StrongAuthenticationUserDetails.PhoneNumber
-    $PrimarySMTP = $User.ProxyAddresses | Where-Object { $_ -clike "SMTP*" } | ForEach-Object { $_ -replace "SMTP:", "" }
-    $Aliases = $User.ProxyAddresses | Where-Object { $_ -clike "smtp*" } | ForEach-Object { $_ -replace "smtp:", "" }
+    $PrimarySMTP = $User.ProxyAddresses | Where-Object { $_ -clike "SMTP:*" } | ForEach-Object { $_ -replace "SMTP:", "" }
+    $Aliases = $User.ProxyAddresses | Where-Object { $_ -clike "smtp:*" } | ForEach-Object { $_ -replace "smtp:", "" }
 
-    If ($User.StrongAuthenticationRequirements) {
-        $MFAState = $User.StrongAuthenticationRequirements.State
-    }
-    Else {
-        $MFAState = 'Disabled'
+    $MFAState = if ($User.StrongAuthenticationRequirements) {
+        $User.StrongAuthenticationRequirements.State
+    } else {
+        'Disabled'
     }
 
-    If ($MFADefaultMethod) {
-        Switch ($MFADefaultMethod) {
-            "OneWaySMS" { $MFADefaultMethod = "Text code authentication phone" }
-            "TwoWayVoiceMobile" { $MFADefaultMethod = "Call authentication phone" }
-            "TwoWayVoiceOffice" { $MFADefaultMethod = "Call office phone" }
-            "PhoneAppOTP" { $MFADefaultMethod = "Authenticator app or hardware token" }
-            "PhoneAppNotification" { $MFADefaultMethod = "Microsoft authenticator app" }
-        }
-    }
-    Else {
-        $MFADefaultMethod = "Not enabled"
+    $MFADefaultMethod = switch ($MFADefaultMethod) {
+        "OneWaySMS" { "Text code authentication phone" }
+        "TwoWayVoiceMobile" { "Call authentication phone" }
+        "TwoWayVoiceOffice" { "Call office phone" }
+        "PhoneAppOTP" { "Authenticator app or hardware token" }
+        "PhoneAppNotification" { "Microsoft authenticator app" }
+        default { "Not enabled" }
     }
   
     $ReportLine = [PSCustomObject] @{
@@ -50,4 +46,3 @@ ForEach ($User in $Users) {
 Write-Host "Report is in c:\temp\MFAUsers.csv"
 $Report | Select-Object UserPrincipalName, DisplayName, MFAState, MFADefaultMethod, MFAPhoneNumber, PrimarySMTP, Aliases | Sort-Object UserPrincipalName | Out-GridView
 $Report | Sort-Object UserPrincipalName | Export-CSV -Encoding UTF8 -NoTypeInformation c:\temp\MFAUsers.csv
-
