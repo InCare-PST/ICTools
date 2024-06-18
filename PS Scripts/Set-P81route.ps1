c4064c48c879f25075cfedbb6f35352154b0aa23
Drethos <47424284+Drethos@users.noreply.github.com>
2024-06-18 08:09:43 -0500

    adding new p81 script

---
+++ PS Scripts/Set-P81route.ps1
@@ -0,0 +1,22 @@
+functiion Set-P81routes{
+    [CmdletBinding(SupportsShouldProcess = $True)]
+    param (
+
+        [string]$FQDNS,
+
+        [switch]$import = $false,
+
+        [string]$path
+
+    )
+    
+    begin{
+
+    }
+    process{
+
+    }
+    End{
+
+    }
+}
