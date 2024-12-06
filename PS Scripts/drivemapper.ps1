43fe1d96b076ab7e57d0a3058379a26fb4f3fde7
BigDogJT <justin@gallups.net>
2024-12-06 10:58:31 -0600

    add first run

--- PS Scripts/drivemapper.ps1
+++ PS Scripts/drivemapper.ps1
@@ -1,4 +1,24 @@
-#Put together for Kittyhawk NAS Shares
+<#
+ drivemapper.ps1
+
+ By Justin Gallups, Thrive Networks. Use at your own risk.  No warranties are given.
+
+.DISCLAIMER
+    THIS CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ 
+.SYNOPSIS 
+    Kittyhawk NAS Shares drive map
+
+.DESCRIPTION
+    Allows users to map, save, test, delete network shares for their NAS device. 
+
+#>
 param(
 [switch]$Netuse,
 [switch]$kaseya,
@@ -12,7 +32,12 @@ param(
 #Variables
 $sfolder = "$env:USERPROFILE\Secure"
 $sfile = "$sfolder\thrive.xml"
-$adip = '10.10.0.4'
+$sfirst = "$sfolder\firstrun.txt"
+$eds = 'eds.kittyhawkinc.com'
+$syn = 'kh-alb.synology.me'
+$edsip = '10.10.0.*'
+$synip = '192.168.160.*'
+
 
 #Set drives to map here, drive letter, path, label
 $driveMaps = @(
@@ -32,26 +57,49 @@ $driveMaps = @(
         Label = "Archive Drive"
     }#>
 )
-
 function OldSchool{
-    #Do not use this unless it is an emergency as password will be visable in clear text
+    #Do not use this unless it is an emergency as password will be visable in clear text.
+    #This is for testing and troubleshooting ONLY!
+
     #Cleanup old drives
+    foreach($d in $drivemaps){
+        net use "$($d.letter):" /del /y 2>null
+        net use "$($d.path)" /del /y 2>null
+        }
+<#
+    #Manual deletion if the loop fails
     net use h: /del /y 2>null
     net use p: /del /y 2>null
     net use y: /del /y 2>null
     net use "\\kh-alb.synology.me\G-Drive" /del /y 2>null
     net use "\\kh-alb.synology.me\Public" /del /y 2>null
     net use "\\kh-alb.synology.me\Archive" /del /y 2>null
-
+#>
     #Temp Save Creds
     $User = Read-Host -Prompt 'Enter an email address'
-    $PWord = Read-Host -Prompt 'Enter a Password'
+    $PWord = Read-Host -Prompt 'Enter a Password' -MaskInput
     
     #Map new drives
+    foreach($d in $drivemaps){
+        write-host "Mapping drive $($d.letter)"
+        try{
+            net use "$($d.letter):" "$($d.path)" $PWord /user:$user /PERSISTENT:YES
+        }catch{
+            Add-Type -AssemblyName PresentationCore,PresentationFramework
+            $ButtonType = [System.Windows.MessageBoxButton]::OK
+            $MessageboxTitle = “Misc error”
+            $Messageboxbody = “Unable to map drive $($drive.Letter): `n$($_.Exception.Message) `n$(get-date)”
+            $MessageIcon = [System.Windows.MessageBoxImage]::Error
+            [System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
+
+    }
+    }
+<#
+    #Might as well be manually mapped 
     net use h: "\\kh-alb.synology.me\G-Drive" $PWord /user:$user
     net use p: "\\kh-alb.synology.me\Public" $PWord /user:$user
-    #net use y: "\\kh-alb.synology.me\Archive" $PWord /user:$user
-
+    net use y: "\\kh-alb.synology.me\Archive" $PWord /user:$user
+#>
     #Force clear creds
     $user = $null
     $PWord = $null
@@ -66,18 +114,18 @@ function ShowDrives{
     }
 
 function Delete{
-    write-host "Deleteing Drives"
-    net use h: /del /y 2>null
-    net use p: /del /y 2>null
-    net use y: /del /y 2>null
-    net use "\\kh-alb.synology.me\G-Drive" /del /y 2>null
-    net use "\\kh-alb.synology.me\Public" /del /y 2>null
-    net use "\\kh-alb.synology.me\Archive" /del /y 2>null
+    foreach($d in $drivemaps){
+        write-host "Deleteing drive $($d.letter):"
+        net use "$($d.letter):" /del /y 2>null
+        write-host "Deleteing path $($d.path)"
+        net use "$($d.path)" /del /y 2>null
+        }
     Exit
 }
 
 function ClearSaved{
     Remove-item -Path $sfile
+    Remove-item -Path $sfirst
 }
 
 function Save{
@@ -85,18 +133,37 @@ function Save{
     if(test-path $sfile){write-host "Credentials already saved on this computer, if not working use [clearsaved] flag"}
     else {
     Get-Credential | export-clixml -Path $sfile
-    attrib.exe +h +s $sfile
-    attrib.exe +h +s $sfolder
-    }
+    attrib.exe +h $sfile
+    attrib.exe +h $sfolder
+   }
 }
 
 function TestConnection{
-    if(!(Test-Connection $adip -Count 1 -Quiet)){
+    $resolveeds = Resolve-DnsName $eds
+    $resolvesyn = Resolve-DnsName $syn
+    if($resolveeds.ipaddress -notlike $edsip){
         Add-Type -AssemblyName System.Windows.Forms | Out-Null
-        $box = [System.Windows.Forms.MessageBox]::Show("Can not communicate with Azure AD, please add routes or use local DNS.","Routing Error!",[System.Windows.Forms.MessageBoxButtons]::OK)
+        $box = [System.Windows.Forms.MessageBox]::Show("Can not communicate with Azure AD, please add routes or use local DNS. Currently resolves to: $($resolveeds.ipaddress)","Routing Error!",[System.Windows.Forms.MessageBoxButtons]::OK)
+    }
+    if($box -eq 'OK'){Exit}
+    if($resolvesyn.ipaddress -notlike $synip){
+        Add-Type -AssemblyName System.Windows.Forms | Out-Null
+        $box = [System.Windows.Forms.MessageBox]::Show("Can not communicate with Synology NAS, please add routes or use local DNS. Currently resolves to: $($resolvesyn.ipaddress)","Routing Error!",[System.Windows.Forms.MessageBoxButtons]::OK)
     }
     if($box -eq 'OK'){Exit}
     
+}
+
+function FirstRun{
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
+    $ButtonType = [System.Windows.MessageBoxButton]::YesNo
+    $MessageboxTitle = “Save Prompt”
+    $Messageboxbody = “This is your first time mapping the drives, would you like to save your credentials?”
+    $MessageIcon = [System.Windows.MessageBoxImage]::Question
+    $answer = [System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
+    if(!(test-path $sfolder)){mkdir $sfolder}
+    get-date | out-file $sfirst
+    if($answer -eq "Yes"){Save}
 }
 
 TestConnection
@@ -106,13 +173,11 @@ if($delete){Delete}
 if($netuse){OldSchool}
 if($save){Save}
 if($clearsaved){ClearSaved}
+if(!(test-path $sfirst)){FirstRun}
 
 
 
-
-#if(Test-Connection -ComputerName eds.kittyhawkinc.com -Count 1 -Quiet){
 #Credentials collection
-
 if([bool]$kaseya){
     $User = Read-Host -Prompt 'Enter an email address'
     $PWord = Read-Host -Prompt 'Enter a Password' -AsSecureString
@@ -132,7 +197,34 @@ foreach($drive in $driveMaps){
         cmd.exe /c "net use $($drive.letter): /del /y 2>null"
         start-sleep -Seconds 2
     }
-    $null = New-PSDrive -PSProvider FileSystem -Name $drive.Letter -Root $drive.Path -Description $drive.Label -Credential $creds -Scope global -ErrorAction Stop -Persist
-
-}
+    try{
+        $null = New-PSDrive -PSProvider FileSystem -Name $drive.Letter -Root $drive.Path -Description $drive.Label -Credential $creds -Scope global -ErrorAction Stop -Persist
+    }catch{
+        if($_.Exception.Message -eq "The specified network password is not correct."){
+            #Write-Host "Unable to map drive due to invalid credentials."
+            Add-Type -AssemblyName PresentationCore,PresentationFramework
+            $ButtonType = [System.Windows.MessageBoxButton]::OK
+            $MessageboxTitle = “Password error”
+            $Messageboxbody = “Your password is incorrect, clearing your saved credentials”
+            $MessageIcon = [System.Windows.MessageBoxImage]::Warning
+            $answer = [System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
+        }
+        if($answer -eq 'OK'){
+            ClearSaved
+            Exit
+        }
+        }else{
+            #Write-Host "Unable to map drive $($drive.Letter):"
+            #Write-Error $_.Exception.Message 
+            Add-Type -AssemblyName PresentationCore,PresentationFramework
+            $ButtonType = [System.Windows.MessageBoxButton]::OK
+            $MessageboxTitle = “Misc error”
+            $Messageboxbody = “Unable to map drive $($drive.Letter): `n$($_.Exception.Message)”
+            $MessageIcon = [System.Windows.MessageBoxImage]::Warning
+            $answer = [System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
+                if($answer -eq 'OK'){
+                    Exit
+                }
+             }
+        }
 
