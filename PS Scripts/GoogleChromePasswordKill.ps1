d2acfe5b07171d36c31e4d0159d8e86187a501b3
BigDogJT <justin@gallups.net>
2025-01-14 14:36:42 -0600

    GoogleChromeKillPasswords

---
+++ PS Scripts/GoogleChromePasswordKill.ps1
@@ -0,0 +1,17 @@
+#Script to clear Google Chrome Passwords
+
+Function DeletePasswords {
+    $users = Get-ChildItem -Path c:\users -Directory
+    ForEach ($user in $users) {    
+            remove-item "C:\Users\$($user.name)\AppData\Local\Google\Chrome\User Data\Default\Login Data" -Force 
+        }
+}
+
+Function KillChrome {
+    Stop-Process -Name Chrome -Force
+}
+
+
+KillChrome
+DeletePasswords
+
