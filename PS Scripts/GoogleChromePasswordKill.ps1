2e6b1e4294ade426b8ed0718d7126371a180284e
BigDogJT <justin@gallups.net>
2025-01-14 14:44:05 -0600

    silentlycontinue

--- PS Scripts/GoogleChromePasswordKill.ps1
+++ PS Scripts/GoogleChromePasswordKill.ps1
@@ -3,7 +3,7 @@
 Function DeletePasswords {
     $users = Get-ChildItem -Path c:\users -Directory
     ForEach ($user in $users) {    
-            remove-item "C:\Users\$($user.name)\AppData\Local\Google\Chrome\User Data\Default\Login Data" -Force 
+            remove-item "C:\Users\$($user.name)\AppData\Local\Google\Chrome\User Data\Default\Login Data" -Force -ErrorAction SilentlyContinue
         }
 }
 
